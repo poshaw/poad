6f8645e685ffd55c8c652a1542e6612aee2c6541
Phil Shaw <poadshaw@gmail.com>
2012-10-12 09:37:34 -0500

    fixed argument-options.py

--- argument_options.py
+++ argument_options.py
@@ -4,7 +4,8 @@ r"""
 This is the docstring
 for the file.
 
-Hi
+run using:
+$ python3 argument_options.py -vv 5
 """
 
 import argparse, sys
@@ -14,17 +15,18 @@ __version__ = "1.0"
 __copyright__ = "Copyright (c) 2012 Phil Shaw"
 __license__ = "GPL"
 
-# define and process command options
-parser = argparse.ArgumentParser(description='{}'.format(__doc__))
-parser.add_argument('-v', '--verbose', action='count', help='provides more output durring execution. -vv will display even more...')
-parser.add_argument('--version', action='version', version='%(prog)s {}'.format(__version__))
-parser.add_argument('-f', '--foo', action='store_true', help='an extra argument for reference') # foo is bool
-parser.add_argument('bar', type=int, help='an extra argument for reference')
 
 
-opts = parser.parse_args()
 
 def main(args):
+	# define and process command options
+	parser = argparse.ArgumentParser(description='{}'.format(__doc__))
+	parser.add_argument('-v', '--verbose', action='count', help='provides more output durring execution. -vv will display even more...')
+	parser.add_argument('--version', action='version', version='%(prog)s {}'.format(__version__))
+	parser.add_argument('-f', '--foo', action='store_true', help='an extra argument for reference') # foo is bool
+	parser.add_argument('bar', type=int, help='an extra argument for reference')
+	opts = parser.parse_args()
+
 	if opts.verbose:
 		print("verbose")
 
